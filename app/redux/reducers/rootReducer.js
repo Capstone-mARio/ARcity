72f52757435374cd4cb671682d39801c311a12e0
AsherLMac <renpsybeta@gmail.com>
2018-08-15 15:29:37 -0400

    Swap Components To Redux

--- app/redux/reducers/rootReducer.js
+++ app/redux/reducers/rootReducer.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 
-import authReducer from "./authReducer"
+import authReducer from './authReducer'
+import arCityReducer from './arCityReducer'
 // import { reducer as homeReducer } from "../modules/home"
 
 // Combine all the reducers
-const rootReducer = combineReducers({ authReducer });
+const rootReducer = combineReducers({ authReducer, arCityReducer });
 
 export default rootReducer;
