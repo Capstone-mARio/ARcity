614759b149f5d04e7a0f61a3eac50a7c854c82e4
AsherLMac <renpsybeta@gmail.com>
2018-08-17 11:02:51 -0400

    Collect Coins

---
+++ app/components/ARComponents/CollectCoins.js
@@ -0,0 +1,57 @@
+//React Imports
+import React, { Components } from 'react'
+import {
+  ViroARScene,
+} from 'react-viro'
+
+class CollectCoins extends Components {
+  constructor() {
+    super();
+    this.state = {
+      coinNum: 0,
+      coinSpawn: 5000
+    }
+    this._addCoin = this._addCoin.bind(this);
+    this._makeCoins = this._makeCoins.bind(this);
+    this._displayCoins = this._displayCoins.bind(this);
+  }
+
+  render() {
+    return (
+      <ViroARScene>
+        {this._displayCoins()}
+      </ViroARScene>
+    )
+  }
+
+  _addCoin() {
+    setTimeout(function () {
+      this.setState({
+        coinNum: coinNum + 1
+      })
+    }, this.state.coinSpawn)
+  }
+
+  _makeCoins() {
+    var coins = []
+    for (let i = 0; i < this.state.coinNum; coins++) {
+      var coin = <ViroBox
+        scale={[1, 1, 1]}
+        position={[0, 1, -1]}
+        viroTag="coin"
+      />
+      coins.push(coin);
+    }
+    return coins;
+  }
+
+  _displayCoins() {
+    return (
+      <ViroNode>
+        {this._makeCoins()}
+      </ViroNode>
+    )
+  }
+}
+
+export default CollectCoins;
